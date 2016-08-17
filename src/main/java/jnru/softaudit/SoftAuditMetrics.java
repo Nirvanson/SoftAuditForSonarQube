e56b5c192da24eb48e49a568311194db152f0c85
Nirvanson <Nirvanson@gmx.de>
2016-08-17 22:40:48 +0200

    Project Setup

---
+++ src/main/java/jnru/softaudit/SoftAuditMetrics.java
@@ -0,0 +1,17 @@
+package jnru.softaudit;
+
+import java.util.List;
+
+import org.sonar.api.measures.Metric;
+import org.sonar.api.measures.Metrics;
+
+public class SoftAuditMetrics implements Metrics{
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public List<Metric> getMetrics() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/jnru/softaudit/SoftAuditPluginTest.java
@@ -0,0 +1,17 @@
+package jnru.softaudit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SoftAuditPluginTest {
+
+    public SoftAuditPluginTest() {
+        super();
+    }
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
